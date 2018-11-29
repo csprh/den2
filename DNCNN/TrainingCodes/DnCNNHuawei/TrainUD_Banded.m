128f8e81183f42ed8a4468cbeb34c4ba48fba722
csprh <paul.hill@bristol.ac.uk>
2018-11-29 23:36:30 +0000

    update

--- DNCNN/TrainingCodes/DnCNNHuawei/TrainUD_Banded.m
+++ DNCNN/TrainingCodes/DnCNNHuawei/TrainUD_Banded.m
@@ -19,7 +19,7 @@ modelName = fullfile(modelBase,['model_',num2str(theseClasses),'_',num2str(these
 %%%-------------------------------------------------------------------------
 opts.modelName        = modelName; %%% model name
 opts.learningRate     = [logspace(-3,-3,30) logspace(-4,-4,20)];%%% you can change the learning rate
-opts.learningRate     = [logspace(-3,-3,30) logspace(-3,-5,30)];
+opts.learningRate     = [logspace(-3,-3,30) logspace(-3,-5,30) logspace(-3,-4,30) logspace(-3,-5,30)];
 %opts.learningRate     = [logspace(-3,-3,12) logspace(-4,-4,7)];
 opts.batchSize        = 128;
 opts.gpus             = [1]; %%% this code can only support one GPU!
