2c38e819a6640d25645e36da9ec0ea266ba0c14f
csprh <paul.hill@bristol.ac.uk>
2019-06-03 13:41:36 +0100

    update

---
+++ testCode/inSarTest.m
@@ -0,0 +1,67 @@
+function inSarTest;
+gpuDevice();
+
+run /home/cosc/csprh/linux/code/matconvnet-1.0-beta25/matlab/vl_setupnn
+
+
+scratchDir = '/space/csprh/inSAR';
+inputDir  = [scratchDir '/outgrad/'];
+deformDir  = [scratchDir '/grad/'];
+wrappedDir  = [scratchDir '/out/'];
+testDir = [scratchDir '/test/'];
+hatDir = [scratchDir '/deformHat/'];
+
+modName = '/home/cosc/csprh/linux/code/den2/DnCNN_inSAR/TrainingCodes/DnCNN_InSAR/data/model_InSAR_1_0/model_InSAR_1_0-epoch-70.mat';
+
+thisInd = 0;
+for ii = 350 : 350+50
+    outPSNR(ii) = thisDenoise(ii, deformDir, inputDir, wrappedDir, modName);
+end
+outPSNRMean = mean(outPSNR);
+
+function PSNR = thisDenoise(ImNum, deformDir, inputDir,wrappedDir, hatDir,  nameOfModel)
+
+load(fullfile(nameOfModel));
+
+net = vl_simplenn_tidy(net);
+net.layers = net.layers(1:end-1);
+net = vl_simplenn_tidy(net);
+
+numStrOutImGrad = sprintf([inputDir 'OOO%05d.png'],ii);
+numStrOutDeform = sprintf([deformDir 'D%05d.png'],ii);
+numStrOutWrappedIn = sprintf([wrappedDir 'O%05d.png'],ii); 
+outStr = sprintf([hatDir 'H%05d.png'],ii);
+
+zRGB = im2double(imread(numStrOutImGrad));
+yRGB = im2double(imread(numStrOutDeform));
+wRGB = im2double(imread(numStrOutWrappedIn));
+input = single(zRGB);
+clean = single(yRGB);
+
+res    = vl_simplenn(net,input,[],[],'conserveMemory',true,'mode','test','CuDNN', 'true');
+
+outIm = input - res(end).x;
+
+scaledgx = double(outIm(:,:,1));
+scaledgy = double(outIm(:,:,2));
+
+scaledgx = ((scaledgx/255.0)*(2*pi)))-pi;
+scaledgy = ((scaledgy/255.0)*(2*pi)))-pi;
+
+wrappedPhaseIn = ((wRGB/255.0)*(2*pi)))-pi; 
+
+phat = intgrad2(scaledgx,scaledgy,[],[],wrappedPhaseIn(0));
+phat_wrapped = angle(exp(1i*phat));
+
+outImWrap = round(255*(phat_wrapped+pi)/(2*pi));
+imwrite(uint8(outImWrap), outStr);
+
+refscaledgx = double(clean(:,:,1));
+refscaledgy = double(clean(:,:,2));
+
+refscaledgx = ((refscaledgx/255.0)*(2*pi)))-pi;
+refscaledgy = ((refscaledgy/255.0)*(2*pi)))-pi;
+PSNR = 0;
+%[PSNR, SSIM] = Cal_PSNRSSIM(255*clean,255*outIm,0,0);
+
+
