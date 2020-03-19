dce2e72ec306def37919f9e3573022cbe5a6617f
csprh <paul.hill@bristol.ac.uk>
2020-03-19 16:25:10 +0000

    update

--- DnCNN/Demo_test_DnCNN_C_PRH.m
+++ DnCNN/Demo_test_DnCNN_C_PRH.m
@@ -3,11 +3,14 @@
 %%% The model is trained with 1) noise levels in [0 55]; 2) 432 training images.
 
 %run /Users/csprh/Dlaptop/MATLAB/TOOLBOXES/matconvnet-1.0-beta25/matlab/vl_setupnn
-run /home/cosc/csprh/linux/code/matconvnet/matlab/vl_setupnn
+%run /home/cosc/csprh/linux/code/matconvnet/matlab/vl_setupnn
+run /mnt/storage/home/csprh/code/matconvnet-1.0-beta25/matlab/vl_setupnn
 % clear; clc;
 addpath('utilities');
-folderTest  = '/space/csprh/ICME/PNGS/S1ElFuente-Palacio_1920x1080_60fps_10bit_420';
-folderTestOut = '/space/csprh/ICME/DENOISED1/S1ElFuente-Palacio_1920x1080_60fps_10bit_420'
+%folderTest  = '/space/csprh/ICME/PNGS/S1ElFuente-Palacio_1920x1080_60fps_10bit_420';
+%folderTestOut = '/space/csprh/ICME/DENOISED1/S1ElFuente-Palacio_1920x1080_60fps_10bit_420'
+folderTest  = '/mnt/storage/scratch/csprh/ICME/PNGS/S1ElFuente-Palacio_1920x1080_60fps_10bit_420';
+folderTestOut  = '/mnt/storage/scratch/csprh/ICME/DENOISED1/S1ElFuente-Palacio_1920x1080_60fps_10bit_420';
 %folderTest  = '/Volumes/David_Bull/ICME_GC/ORIG/S1ElFuente-Palacio_1920x1080_60fps_10bit_420'; %%% test dataset
 %folderTestOut = '/Volumes/David_Bull/ICME_GC/DENOISED1/S1ElFuente-Palacio_1920x1080_60fps_10bit_420';
 folderModel = 'model'; 
